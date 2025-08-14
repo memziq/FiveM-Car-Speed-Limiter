b6a29c3a006b629feae7264d2d123a27381e4ce7
memziq <memziqq@gmail.com>
2025-08-14 11:58:02 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,21 @@
+Config = {}
+
+Config.GlobalSpeedLimit = 300
+
+Config.ModelSpeedLimits = {
+    -- You can add custom speed limits for specific vehicle models here.
+    -- Usage example:
+    -- ["sultan"] = 150,  -- Sets the speed limit for the Sultan model to 150 km/h
+    -- ["adder"] = 120,   -- Sets the speed limit for the Adder model to 120 km/h
+}
+
+Config.CategorySpeedLimits = {
+    -- You can add custom speed limits for vehicle categories here.
+    -- Category names must be in uppercase (e.g., "SUV", "SUPER", "SEDAN").
+    -- Usage example:
+    -- ["SUV"] = 110,     -- Sets the speed limit for all SUV vehicles to 110 km/h
+    -- ["SUPER"] = 160,   -- Sets the speed limit for all SUPER vehicles to 160 km/h
+}
+
+-- https://linktr.ee/memziq
+-- https://github.com/memziq
---
+++ src/server.lua
@@ -0,0 +1,2 @@
+print("^2Speed limiter started successfully!^0")
+print("^2https://linktr.ee/memziq^0")
